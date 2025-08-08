51e09d9c2903cb213f248c6b58535abf99632ddc
pavelbogusevskij58-commits <pavelbogusevskij58@gmail.com>
2025-08-09 00:17:26 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # kartofel_dolina_bot
+
+Telegram-бот для приёма заказов на картошку.
+
+## Установка
+
+1. Создайте `.env` файл и вставьте токен:
+```
+BOT_TOKEN=ваш_токен_от_BotFather
+```
+
+2. Установите зависимости:
+```
+pip install -r requirements.txt
+```
+
+3. Запустите:
+```
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+import telebot
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "👋 Добро пожаловать в Долину Картофеля!
+
+Напиши /menu чтобы сделать заказ.")
+
+@bot.message_handler(commands=['menu'])
+def show_menu(message):
+    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add("🥔 Заказать картофель", "📞 Связаться с фермером")
+    bot.send_message(message.chat.id, "Выберите действие:", reply_markup=markup)
+
+@bot.message_handler(func=lambda message: True)
+def handle_all(message):
+    if message.text == "🥔 Заказать картофель":
+        bot.send_message(message.chat.id, "Отлично! Напишите, сколько сеток картофеля вы хотите заказать.")
+    elif message.text == "📞 Связаться с фермером":
+        bot.send_message(message.chat.id, "Фермер Павел: +7 923 456-78-90")
+    else:
+        bot.send_message(message.chat.id, "Не понял команду. Напиши /menu.")
+
+bot.polling(none_stop=True)
